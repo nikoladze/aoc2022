a3ef9ff86c5784d7d9563da70268ed512b6b334a
Nikolai Hartmann <nikoladze@posteo.de>
2022-12-03 08:32:30 +0100

    init logging in main

--- init.py
+++ init.py
@@ -9,9 +9,6 @@ from datetime import date
 import logging
 from argparse import ArgumentParser
 
-logging.basicConfig(
-    level=logging.INFO, format="%(levelname)-8s %(funcName)s: %(message)s",
-)
 logger = logging.getLogger(__name__)
 
 
@@ -67,6 +64,10 @@ def get_input(day, year):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname)-8s %(funcName)s: %(message)s",
+    )
+
     parser = ArgumentParser()
     parser.add_argument("-d", "--day", default=date.today().day, type=int)
     parser.add_argument("-y", "--year", default=date.today().year, type=int)
