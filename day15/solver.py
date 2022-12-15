e21193f61c6d2f35e50f6c34bb8736250fbf2667
Nikolai Hartmann <nikoladze@posteo.de>
2022-12-15 10:39:49 +0100

    add solution for day15 part2

--- day15/solver.py
+++ day15/solver.py
@@ -30,6 +30,7 @@ def print_map(data, is_not_at):
     min_y = min(x for x, y in all_points)
     max_x = max(y for x, y in all_points)
     max_y = max(y for x, y in all_points)
+    print(min_x, max_x)
     for y in range(min_y, max_y + 1):
         for x in range(min_x, max_x + 1):
             pos = (x, y)
@@ -55,8 +56,8 @@ def solve1(data, where_y=2000000):
         dx_sb = abs(sensor[0] - beacon[0])
         dy_sb = abs(sensor[1] - beacon[1])
         distance = dx_sb + dy_sb
-        #for y in range(sensor[1] - distance, sensor[1] + distance + 1):
-        for y in [where_y]:
+        for y in range(sensor[1] - distance, sensor[1] + distance + 1):
+        #for y in [where_y]:
             for x in range(sensor[0] - distance, sensor[0] + distance + 1):
                 if (x, y) in [sensor, beacon]:
                     continue
@@ -65,19 +66,69 @@ def solve1(data, where_y=2000000):
                 if (dx + dy) <= distance:
                     is_not_at.add((x, y))
     #print(len(is_not_at))
-    #print_map(data, is_not_at)
+    if where_y == 10:
+        print_map(data, is_not_at)
     return len([(x, y) for x, y in is_not_at if y == where_y])
+
+
+def find_not_covered(ranges, xmin, xmax):
+    not_covered = [(xmin, xmax)]
+    for xmin, xmax in ranges:
+        new_not_covered = []
+        for xmin_nc, xmax_nc in not_covered:
+            if xmin_nc < xmin <= xmax_nc and xmax >= xmax_nc:
+                #      |    |
+                #         ------
+                # ->   | |
+                new_not_covered.append((xmin_nc, xmin - 1))
+            elif xmin_nc < xmin < xmax_nc and xmin_nc < xmax < xmax_nc:
+                #   |     |
+                #     ---
+                # ->||   ||
+                new_not_covered += [(xmin_nc, xmin - 1), (xmax + 1, xmax_nc)]
+            elif xmin <= xmin_nc and xmin_nc <= xmax < xmax_nc:
+                #       |     |
+                #     ----
+                # ->      |   |
+                new_not_covered.append((xmax + 1, xmax_nc))
+            elif xmin <= xmin_nc <= xmax and xmin <= xmax_nc <= xmax:
+                #  |   |
+                # -------
+                # -> this range is now completely covered
+                continue
+            else:
+                # i think now we are not overlapping and don't give additional info
+                # so we need to add the range again
+                new_not_covered.append((xmin_nc, xmax_nc))
+        not_covered = new_not_covered
+    return not_covered
+
+
 
 
 # PART 2
 @measure_time
-def solve2(data):
-    pass
+def solve2(data, ymax=4000000):
+    for y in tqdm(range(0, ymax)):
+        ranges = []
+        for sensor, beacon in data:
+            dx_sb = abs(sensor[0] - beacon[0])
+            dy_sb = abs(sensor[1] - beacon[1])
+            distance = dx_sb + dy_sb
+            dx_max = distance - abs(y - sensor[1])
+            if dx_max > 0:
+                ranges.append((sensor[0] - dx_max, sensor[0] + dx_max))
+        not_covered = find_not_covered(ranges, 0, ymax)
+        if y == 1:
+            print(ranges)
+        if not_covered:
+            print(not_covered)
+            return not_covered[0][0] * 4000000 + y
 
 
 if __name__ == "__main__":
     data = parse(open(Path(__file__).parent / "input.txt").read())
-    print("Part 1: {}".format(solve1(data)))
+    #print("Part 1: {}".format(solve1(data)))
     print("Part 2: {}".format(solve2(data)))
 
     print("\nTime taken:")
--- day15/test_solver.py
+++ day15/test_solver.py
@@ -1,5 +1,5 @@
 import pytest
-from solver import parse, solve1, solve2
+from solver import parse, solve1, solve2, find_not_covered
 
 TESTDATA = """
 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -36,5 +36,21 @@ def test_solve1(parsed_data):
 
 # PART 2
 def test_solve2(parsed_data):
-    solution = solve2(parsed_data)
-    # asserts go here
+    solution = solve2(parsed_data, ymax=20)
+    assert solution == 56000011
+
+
+def test_not_covered():
+    assert find_not_covered([(0, 10), (20, 25)], 8, 30) == [(11, 19), (26, 30)]
+    assert find_not_covered([(-1, 5)], 0, 10) == [(6, 10)]
+    assert find_not_covered([(2, 5)], 0, 10) == [(0, 1), (6, 10)]
+    assert find_not_covered([(5, 15)], 0, 10) == [(0, 4)]
+    assert find_not_covered([(-1, 11)], 0, 10) == []
+    assert find_not_covered([(-30, -20)], 0, 10) == [(0, 10)]
+    assert find_not_covered([(5, 5)], 0, 10) == [(0, 4), (6, 10)]
+    assert find_not_covered([(-3, 5), (7, 12)], 0, 10) == [(6, 6)]
+    assert find_not_covered([(-3, 0)], 0, 10) == [(1, 10)]
+    assert find_not_covered([(10, 15)], 0, 10) == [(0, 9)]
+    assert find_not_covered([(10, 15)], 10, 10) == []
+    assert find_not_covered([(-10, 5), (20, 30), (40, 50), (60, 70), (88, 88), (90, 110)], 0, 100) == [(6, 19), (31, 39), (51, 59), (71, 87), (89, 89)]
+    assert find_not_covered([(-10, 50), (92, 200), (40, 70), (50, 90)], 0, 100) == [(91, 91)]
