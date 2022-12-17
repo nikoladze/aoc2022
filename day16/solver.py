975454a7e18c0d65de8807c9f5107b9c6cf07b0d
Nikolai Hartmann <nikoladze@posteo.de>
2022-12-17 15:35:34 +0100

    add solution for day16

---
+++ day16/solver.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+import sys
+from pathlib import Path
+
+import utils
+
+
+measure_time = utils.stopwatch()
+
+
+@measure_time
+def parse(raw_data):
+    out = []
+    for line in raw_data.splitlines():
+        valve, destinations = line.strip().split("; ")
+        words = valve.split()
+        valve = words[1]
+        rate = int(words[4].split("=")[1])
+        destinations = " ".join(destinations.split()[4:])
+        destinations = tuple(destinations.split(", "))
+        out.append(((valve, rate), destinations))
+    return out
+
+
+# PART 1
+@measure_time
+def solve1(data):
+    destinations = {}
+    rates = {}
+    for (valve, rate), valve_destinations in data:
+        destinations[valve] = valve_destinations
+        rates[valve] = rate
+
+    for valve, valve_destinations in list(destinations.items()):
+        destinations[valve] = sorted(
+            valve_destinations, key=lambda x: rates[x], reverse=True
+        )
+
+    total_minutes = 30
+    state_scores = {}
+
+    def search(pos="AA", open_valves=frozenset(), score=0, time=0):
+        if time >= total_minutes:
+            return score
+        try:
+            prev_score, prev_time = state_scores[(pos, open_valves)]
+            if prev_score >= score and prev_time <= time:
+                # we've been here *and* it took same or less time
+                # *and* we had a same or better score
+                return score
+        except KeyError:
+            pass
+        state_scores[(pos, open_valves)] = (score, time)
+
+        scores = []
+        for action in [OPEN] + list(destinations[pos]):
+            new_valves = open_valves
+            dest = pos
+            valve_score = 0
+            if action == OPEN:
+                if pos in open_valves:
+                    continue
+                if rates[pos] == 0:
+                    continue
+                new_valves = new_valves | {pos}
+                valve_score += rates[pos] * (total_minutes - (time + 1))
+            else:
+                dest = action
+            scores.append(search(dest, new_valves, score + valve_score, time + 1))
+
+        return max(scores + [0])
+
+    return search()
+
+
+OPEN = 1
+
+# PART 2
+@measure_time
+def solve2(data):
+    destinations = {}
+    rates = {}
+    for (valve, rate), valve_destinations in data:
+        destinations[valve] = valve_destinations
+        rates[valve] = rate
+
+    for valve, valve_destinations in list(destinations.items()):
+        destinations[valve] = sorted(
+            valve_destinations, key=lambda x: rates[x], reverse=True
+        )
+
+    total_minutes = 26
+    state_scores = {}
+
+    def search(pos_me="AA", pos_elefant="AA", open_valves=frozenset(), score=0, time=0):
+        #print(f"{pos_me=}, {pos_elefant=}, {open_valves=}, {score=}, {time=}")
+        if time >= total_minutes:
+            return score
+        try:
+            prev_score, prev_time = state_scores[(pos_me, pos_elefant, open_valves)]
+            if prev_score >= score and prev_time <= time:
+                # we've been here *and* it took same or less time
+                # *and* we had a same or better score
+                return score
+        except KeyError:
+            pass
+        state_scores[(pos_me, pos_elefant, open_valves)] = (score, time)
+
+        scores = []
+        for my_action in [OPEN] + list(destinations[pos_me]):
+            for elefant_action in [OPEN] + list(destinations[pos_elefant]):
+                new_valves = open_valves
+                dest_me = pos_me
+                dest_elefant = pos_elefant
+                valve_score = 0
+                # elefant
+                if elefant_action == OPEN:
+                    if pos_elefant in open_valves:
+                        continue
+                    if rates[pos_elefant] == 0:
+                        continue
+                    new_valves = new_valves | {pos_elefant}
+                    valve_score += rates[pos_elefant] * (total_minutes - (time + 1))
+                else:
+                    dest_elefant = elefant_action
+                # me
+                if my_action == OPEN:
+                    if pos_me in open_valves:
+                        continue
+                    if rates[pos_me] == 0:
+                        continue
+                    new_valves = new_valves | {pos_me}
+                    valve_score += rates[pos_me] * (total_minutes - (time + 1))
+                else:
+                    dest_me = my_action
+                #print(f"{my_action=}, {elefant_action=}")
+                scores.append(
+                    search(
+                        dest_me, dest_elefant, new_valves, score + valve_score, time + 1
+                    )
+                )
+
+        return max(scores + [0])
+
+    res = search()
+    print(len(state_scores))
+    return res
+
+
+if __name__ == "__main__":
+    data = parse(open(Path(__file__).parent / "input.txt").read())
+    print("Part 1: {}".format(solve1(data)))
+    #print("Part 2: {}".format(solve2(data)))
+
+    print("\nTime taken:")
+    for func, time in measure_time.times:
+        print(f"{func:8}{time}s")
+    print("----------------")
+    print("total   {}s".format(sum(t for _, t in measure_time.times)))
---
+++ day16/test_solver.py
@@ -0,0 +1,36 @@
+import pytest
+from solver import parse, solve1, solve2
+
+TESTDATA = """
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+"""
+
+@pytest.fixture
+def parsed_data():
+    return parse(TESTDATA.strip())
+
+
+def test_parse():
+    data = parse(TESTDATA.strip())
+    # asserts go here
+
+
+# PART 1
+def test_solve1(parsed_data):
+    solution = solve1(parsed_data)
+    assert solution == 1651
+
+
+# PART 2
+def test_solve2(parsed_data):
+    solution = solve2(parsed_data)
+    assert solution == 1707
