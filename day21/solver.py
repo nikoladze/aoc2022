033094d1fa5bdab3a2779fcff8a70e08f1efda06
Nikolai Hartmann <nikoladze@posteo.de>
2022-12-21 12:07:08 +0100

    cleanup

--- day21/solver.py
+++ day21/solver.py
@@ -20,76 +20,73 @@ def parse(raw_data):
     return out
 
 
+class MonkeyCalculator:
+    def __init__(self, data):
+        self.dag = {}
+        self.initial_results = {}
+        self.operation = {}
+        for monkey, job in data:
+            if len(job) > 1:
+                self.dag[monkey] = [job[0], job[2]]
+                self.operation[monkey] = {
+                    "+": op.add,
+                    "-": op.sub,
+                    "*": op.mul,
+                    "/": op.truediv,
+                }[job[1]]
+            else:
+                self.initial_results[monkey] = int(job[0])
+
+    def eval(self, start="root", results=None):
+        if results is None:
+            results = self.initial_results
+        results = results.copy()
+
+        def eval_monkey(monkey):
+            try:
+                return results[monkey]
+            except KeyError:
+                pass
+            a, b = self.dag[monkey]
+            return self.operation[monkey](eval_monkey(a), eval_monkey(b))
+
+        return eval_monkey(start)
+
+    def eval_diff(self, me=0):
+        results = dict(self.initial_results, humn=me)
+        root_monkeys = self.dag["root"]
+        m1, m2 = root_monkeys
+        return self.eval(m1, results) - self.eval(m2, results)
+
+
 # PART 1
 @measure_time
 def solve1(data):
-    dag = {}
-    results = {}
-    operation = {}
-    for monkey, job in data:
-        if len(job) > 1:
-            dag[monkey] = [job[0], job[2]]
-            operation[monkey] = {"+": op.add, "-": op.sub, "*": op.mul, "/": op.truediv}[job[1]]
-        else:
-            results[monkey] = int(job[0])
+    calc = MonkeyCalculator(data)
+    return int(calc.eval())
 
-    def eval_monkey(monkey="root"):
-        try:
-            return results[monkey]
-        except KeyError:
-            pass
-        a, b = dag[monkey]
-        return operation[monkey](eval_monkey(a), eval_monkey(b))
-
-    return eval_monkey()
 
 # PART 2
 @measure_time
 def solve2(data):
-    x1, x2 = 3000000000000, 3100000000000
-    y1, y2 = eval_roots(data, x1)[2], eval_roots(data, x2)[2]
-    a = (y2 - y1) / (x2 - x1)
-    # y1 = a * x1 + b
-    # -> b = y - (a * x1)
-    b = y1 - (a * x1)
-    # 0 = a * x + b
-    # -> x = -b / a
-    x_new = -b / a
-    print(eval_roots(data, x_new))
-    return x_new
-
-def eval_roots(data, me=0):
-    dag = {}
-    results = {}
-    operation = {}
-    for monkey, job in data:
-        if len(job) > 1:
-            dag[monkey] = [job[0], job[2]]
-            operation[monkey] = {"+": op.add, "-": op.sub, "*": op.mul, "/": op.truediv}[job[1]]
-        else:
-            results[monkey] = int(job[0])
-
-    def eval_monkey(monkey="root"):
-        try:
-            return results[monkey]
-        except KeyError:
-            pass
-        a, b = dag[monkey]
-        return operation[monkey](eval_monkey(a), eval_monkey(b))
-
-    root_monkeys = dag["root"]
-    m1, m2 = root_monkeys
-    del dag["root"]
-
-    results["humn"] = me
-    em1, em2 = eval_monkey(m1), eval_monkey(m2)
-    return (em1, em2, em1 - em2)
-
-    # for i in range(1000000):
-    #     results["humn"] = i
-    #     if eval_monkey(m1) == eval_monkey(m2):
-    #         return i
-
+    calc = MonkeyCalculator(data)
+    # Secant method
+    # start with 0, 1
+    x1, x2 = 0, 1
+    y1, y2 = calc.eval_diff(x1), calc.eval_diff(x2)
+    while True:
+        # y1 = a * x1 + b
+        # -> b = y - (a * x1)
+        # 0 = a * x + b
+        # -> x = -b / a
+        a = (y2 - y1) / (x2 - x1)
+        b = y1 - (a * x1)
+        x_new = -b / a
+        y_new = calc.eval_diff(x_new)
+        if y_new == 0:
+            return int(x_new)
+        x1, x2 = x2, x_new
+        y1, y2 = y2, y_new
 
 
 if __name__ == "__main__":
@@ -102,4 +99,3 @@ if __name__ == "__main__":
         print(f"{func:8}{time}s")
     print("----------------")
     print("total   {}s".format(sum(t for _, t in measure_time.times)))
-
