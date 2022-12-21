1fdce5387d3d763f9fbc60fda39d0b6a3cb92483
Nikolai Hartmann <nikoladze@posteo.de>
2022-12-21 09:24:46 +0100

    solution for day21 part2 - sort of

--- day21/solver.py
+++ day21/solver.py
@@ -46,7 +46,50 @@ def solve1(data):
 # PART 2
 @measure_time
 def solve2(data):
-    pass
+    x1, x2 = 3000000000000, 3100000000000
+    y1, y2 = eval_roots(data, x1)[2], eval_roots(data, x2)[2]
+    a = (y2 - y1) / (x2 - x1)
+    # y1 = a * x1 + b
+    # -> b = y - (a * x1)
+    b = y1 - (a * x1)
+    # 0 = a * x + b
+    # -> x = -b / a
+    x_new = -b / a
+    print(eval_roots(data, x_new))
+    return x_new
+
+def eval_roots(data, me=0):
+    dag = {}
+    results = {}
+    operation = {}
+    for monkey, job in data:
+        if len(job) > 1:
+            dag[monkey] = [job[0], job[2]]
+            operation[monkey] = {"+": op.add, "-": op.sub, "*": op.mul, "/": op.truediv}[job[1]]
+        else:
+            results[monkey] = int(job[0])
+
+    def eval_monkey(monkey="root"):
+        try:
+            return results[monkey]
+        except KeyError:
+            pass
+        a, b = dag[monkey]
+        return operation[monkey](eval_monkey(a), eval_monkey(b))
+
+    root_monkeys = dag["root"]
+    m1, m2 = root_monkeys
+    del dag["root"]
+
+    results["humn"] = me
+    em1, em2 = eval_monkey(m1), eval_monkey(m2)
+    return (em1, em2, em1 - em2)
+
+    # for i in range(1000000):
+    #     results["humn"] = i
+    #     if eval_monkey(m1) == eval_monkey(m2):
+    #         return i
+
 
 
 if __name__ == "__main__":
--- day21/test_solver.py
+++ day21/test_solver.py
@@ -1,8 +1,26 @@
 import pytest
 from solver import parse, solve1, solve2
 
-TESTDATA = """
-"""
+TESTDATA = (
+    """
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+    """
+).strip()
+
 
 @pytest.fixture
 def parsed_data():
@@ -17,10 +35,10 @@ def test_parse():
 # PART 1
 def test_solve1(parsed_data):
     solution = solve1(parsed_data)
-    # asserts go here
+    assert solution == 152
 
 
 # PART 2
 def test_solve2(parsed_data):
     solution = solve2(parsed_data)
-    # asserts go here
+    assert solution == 301
