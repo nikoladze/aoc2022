77c96b140e63fa899f3a9dd097ca990b1076d6c5
Nikolai Hartmann <nikoladze@posteo.de>
2022-12-23 08:53:22 +0100

    cleanup

--- day23/solver.py
+++ day23/solver.py
@@ -27,9 +27,7 @@ def print_grid(positions):
         print("\n", end="")
 
 
-# PART 1
-@measure_time
-def solve1(data, rounds=10):
+def solve(data, max_rounds=10000):
     positions = set()
     for y, row in enumerate(data):
         for x, c in enumerate(row):
@@ -44,72 +42,6 @@ def solve1(data, rounds=10):
             ((1, 0), ((1, -1), (1, 0), (1, 1))),
         ]
     )
-
-    # print()
-    # print_grid(positions)
-    # input()
-
-    def no_one_else_around(x, y):
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                if dx == 0 and dy == 0:
-                    continue
-                if (x + dx, y + dy) in positions:
-                    return False
-        return True
-
-    for round in range(rounds):
-        proposed = defaultdict(list)
-        for x, y in positions:
-            if no_one_else_around(x, y):
-                continue
-            for (prop_dx, prop_dy), check_directions in directions:
-                if not any((x + dx, y + dy) in positions for dx, dy in check_directions):
-                    proposed[(x + prop_dx, y + prop_dy)].append((x, y))
-                    break
-        for (x_new, y_new), orig in proposed.items():
-            if len(orig) > 1:
-                continue
-            x, y = orig[0]
-            positions.remove((x, y))
-            positions.add((x_new, y_new))
-        directions.rotate(-1)
-
-        # print_grid(positions)
-        # input()
-
-    xs = [x for x, y in positions]
-    ys = [y for x, y in positions]
-    total = 0
-    for y in range(min(ys), max(ys) + 1):
-        for x in range(min(xs), max(xs) + 1):
-            if not (x, y) in positions:
-                total += 1
-    return total
-
-
-# PART 2
-@measure_time
-@profile
-def solve2(data, max_rounds=10000):
-    positions = set()
-    for y, row in enumerate(data):
-        for x, c in enumerate(row):
-            if c == "#":
-                positions.add((x, y))
-
-    directions = deque(
-        [
-            ((0, -1), ((-1, -1), (0, -1), (1, -1))),
-            ((0, 1), ((-1, 1), (0, 1), (1, 1))),
-            ((-1, 0), ((-1, -1), (-1, 0), (-1, 1))),
-            ((1, 0), ((1, -1), (1, 0), (1, 1))),
-        ]
-    )
-
-    # print()
-    # print_grid(positions)
-    # input()
 
     def no_one_else_around(x, y):
         for dx in [-1, 0, 1]:
@@ -138,9 +70,31 @@ def solve2(data, max_rounds=10000):
             positions.add((x_new, y_new))
         directions.rotate(-1)
         if prev == positions:
-            return round + 1
+            return round + 1, positions
         prev = positions.copy()
 
+    return None, positions
+
+
+# PART 1
+@measure_time
+def solve1(data):
+    _, positions = solve(data, max_rounds=10)
+
+    xs = [x for x, y in positions]
+    ys = [y for x, y in positions]
+    total = 0
+    for y in range(min(ys), max(ys) + 1):
+        for x in range(min(xs), max(xs) + 1):
+            if not (x, y) in positions:
+                total += 1
+    return total
+
+
+# PART 2
+@measure_time
+def solve2(data):
+    return solve(data, max_rounds=10000)[0]
 
 
 if __name__ == "__main__":
